434d1eaf6553c0142ee8ebffd092fb3f97866258
nearHaan <farhaan.nizam675@gmail.com>
2026-01-04 18:46:17 +0530

    login base design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,15 @@
 	//
 </script>
 
-<div>
-	<div>//</div>
+<div class="flex h-screen w-screen flex-col items-center p-x">
+	<h1>Escape Room</h1>
+	<div class="flex h-full items-center">
+		<div class="flex flex-col rounded bg-light-grey p-xs">
+			<p>Team Code</p>
+			<input class="input-primary mb-xxs" />
+			<p>Access Code</p>
+			<input class="input-primary mb-xxs" />
+			<button class="btn-primary">Start</button>
+		</div>
+	</div>
 </div>
